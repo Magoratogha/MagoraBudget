5bbd1e76b2791e1b043193a23c25627a5c93b9cf
Magoratogha <magoratoga@gmail.com>
2026-01-24 16:26:17 -0500

    feat: update Firebase hosting configuration and add service worker

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -2,7 +2,7 @@ import { NavbarItem } from '../models';
 
 export const NAVBAR_ITEMS: NavbarItem[] = [
   { label: 'Inicio', icon: 'bi-house', route: '/home' },
-  { label: 'Transacciones', icon: 'bi-calendar2-week', route: '/home' },
-  { label: 'Cuentas', icon: 'bi-bank', route: '/home' },
-  { label: 'Pendientes', icon: 'bi-card-checklist', route: '/home' },
+  { label: 'Transacciones', icon: 'bi-calendar2-week', route: '/transactions' },
+  { label: 'Cuentas', icon: 'bi-bank', route: '/accounts' },
+  { label: 'Pendientes', icon: 'bi-card-checklist', route: '/pending' },
 ]
