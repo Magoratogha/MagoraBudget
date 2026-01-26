05cedc2ac9f088bf919a853b8634202ed3884d96
Magoratogha <magoratoga@gmail.com>
2026-01-26 18:14:57 -0500

    chore: bump version-info patch to 8

--- version-info.ts
+++ version-info.ts
@@ -1,7 +1,7 @@
 const APP_VERSION = {
   major: 0,
   minor: 0,
-  patch: 7,
+  patch: 8,
 }
 
 export const APP_VERSION_STRING = `${ APP_VERSION.major }.${ APP_VERSION.minor }.${ APP_VERSION.patch }`;
