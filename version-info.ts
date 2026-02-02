9a722aaf1ea920ad851ac5c6286ab68c332e7f35
Magoratogha <magoratoga@gmail.com>
2026-02-02 10:07:04 -0500

    style: update account display styles and improve layout

--- version-info.ts
+++ version-info.ts
@@ -1,7 +1,7 @@
 const APP_VERSION = {
   major: 0,
   minor: 1,
-  patch: 19,
+  patch: 20,
 }
 
 export const APP_VERSION_STRING = `${ APP_VERSION.major }.${ APP_VERSION.minor }.${ APP_VERSION.patch }`;
