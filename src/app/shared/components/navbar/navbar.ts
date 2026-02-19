f9f1c75a0c324567e3d58ea3c754f27706c9c9c5
Magoratogha <magoratoga@gmail.com>
2026-02-18 19:07:48 -0500

    feat: add query date update on navbar item click

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,7 +1,7 @@
 import { Component, computed, inject, input, OnDestroy, OnInit, output } from '@angular/core';
 import { NavbarItem } from '../../models';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { Auth, FireStore } from '../../services';
+import { Auth, FireStore, Query } from '../../services';
 import { Unsubscribe } from '@firebase/firestore';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -20,6 +20,7 @@ import { ACCOUNT_TYPE_INFO_MAP } from '../../../accounts/constants';
 })
 export class Navbar implements OnInit, OnDestroy {
   private _fireStore = inject(FireStore);
+  private _query = inject(Query);
   private _auth = inject(Auth);
   private _unsubscribeFunctions: Unsubscribe[] = [];
 
@@ -49,6 +50,10 @@ export class Navbar implements OnInit, OnDestroy {
 
   onCreateClick() {
     this.createButtonClicked.emit();
+  }
+
+  updateQueryDate() {
+    this._query.updateCurrentDate(new Date());
   }
 
   protected readonly ACCOUNT_TYPE_INFO_MAP = ACCOUNT_TYPE_INFO_MAP;
