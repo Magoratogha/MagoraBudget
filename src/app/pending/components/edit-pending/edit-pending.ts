c6e81c2a0aba7739e4765bc30321789dc3daa670
Magoratogha <magoratoga@gmail.com>
2026-02-25 11:35:52 -0500

    feat: enhance edit-pending form with new fields and validation

--- src/app/pending/components/edit-pending/edit-pending.ts
+++ src/app/pending/components/edit-pending/edit-pending.ts
@@ -1,14 +1,35 @@
-import { Component, inject, signal } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
-import { MatIconButton } from '@angular/material/button';
+import { Component, computed, inject, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
 import { Pending } from '../../models';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { NgxMaskDirective } from 'ngx-mask';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { getAccountTypeIcon, getTransactionTypeIcon } from '../../../shared/utils';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { TransactionType } from '../../../transactions/models';
+import { MatOption } from '@angular/material/core';
+import { MatSelect } from '@angular/material/select';
+import { Query } from '../../../shared/services';
 
 @Component({
   selector: 'app-edit-pending',
   imports: [
-    MatIcon,
-    MatIconButton
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgxMaskDirective,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+    MatOption,
+    MatSelect
   ],
   templateUrl: './edit-pending.html',
   styleUrl: './edit-pending.scss',
@@ -18,9 +39,19 @@ import { Pending } from '../../models';
 })
 export class EditPending {
   private _bottomSheetData = inject(MAT_BOTTOM_SHEET_DATA);
+  private _query = inject(Query);
 
   pending = signal<Pending | undefined>(this._bottomSheetData?.pending);
+  availableOriginAccounts = computed(() => this._query.availableExpensesAccounts());
 
+  protected readonly TransactionType = TransactionType;
+  protected readonly Number = Number;
+  protected readonly getTransactionTypeIcon = getTransactionTypeIcon;
+  protected readonly Object = Object;
+  protected readonly isNaN = isNaN;
 
-  delete() {}
+  delete() {
+  }
+
+  protected readonly getAccountTypeIcon = getAccountTypeIcon;
 }
--- src/app/pending/models/pending.ts
+++ src/app/pending/models/pending.ts
@@ -1,4 +1,11 @@
+import { TransactionType } from '../../transactions/models';
+
 export interface Pending {
   id?: string;
   label: string;
+  amount: number;
+  hasAssociatedTransaction: boolean;
+  transactionType?: TransactionType;
+  originAccountId?: string;
+  targetAccountId?: string;
 }
