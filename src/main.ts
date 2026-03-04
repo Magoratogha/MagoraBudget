a07bde923ff34ac83e3c5f5763778cf63c180a40
Santiago Gómez <sgomez-ecw@cricut.com>
2026-03-04 16:33:01 -0500

    Refactor account handling to remove "Deleted" type

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import "ios-vibrator-pro-max";
+import { enableMainThreadBlocking } from "ios-vibrator-pro-max";
+
+enableMainThreadBlocking(true);
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
